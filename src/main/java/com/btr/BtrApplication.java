54dfe5b27efba07131cdb37cec1adf887d54aa4f
Saloed96 <saloed96@gmail.com>
2020-02-27 20:33:12 +0200

    Added files. first configuration

---
+++ src/main/java/com/btr/BtrApplication.java
@@ -0,0 +1,13 @@
+package com.btr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtrApplication.class, args);
+	}
+
+}
